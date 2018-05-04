ed60e430d74759fa7d20dcef69f6fc4b6af2e766
Rachel <y.rachel.bg@gmail.com>
2018-05-04 14:18:44 -0500

    Solución de conflicto

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,4 @@
 export const environment = {
   production: true,
-<<<<<<< HEAD
-  backend_sdk: 'https://greenroutesdk.xyz:8009/back-sdk/security/login'
-=======
-  backend_sdk: 'https://greenroutesdk.xyz/back-sdk'
->>>>>>> c2b28e4c4559e9cb15bbafcc40ec481d633e51c8
+  backend_sdk: 'https://207.249.127.124:8080/back-sdk/security/login'
 };
